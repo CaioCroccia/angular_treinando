c00edb35d7a56461fb0af62e5f8b6610b1a7bb62
CaioCroccia <103905336+CaioCroccia@users.noreply.github.com>
2022-09-17 01:15:54 -0300

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeaderCaioComponentComponent } from './header-caio-component/header-caio-component.component';
+import { CaioTimerComponentComponent } from './caio-timer-component/caio-timer-component.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderCaioComponentComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderCaioComponentComponent, CaioTimerComponentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/caio-timer-component/caio-timer-component.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caio-timer-component',
+  templateUrl: './caio-timer-component.component.html',
+  styleUrls: ['./caio-timer-component.component.css']
+})
+export class CaioTimerComponentComponent implements OnInit {
+ counter = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  timer = setInterval(() => {
+    this.counter = this.counter + 1;
+  }, 1000);
+
+}
