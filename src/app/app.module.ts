e0a13c505fb9c823246bf74ed0d15f3b65f60da1
CaioCroccia <103905336+CaioCroccia@users.noreply.github.com>
2022-09-17 00:28:39 -0300

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeaderCaioComponentComponent } from './header-caio-component/header-caio-component.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderCaioComponentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/header-caio-component/header-caio-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-caio-component',
+  templateUrl: './header-caio-component.component.html',
+  styleUrls: ['./header-caio-component.component.css']
+})
+export class HeaderCaioComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
