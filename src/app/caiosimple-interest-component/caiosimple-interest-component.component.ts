f51abfe4746f555ecd16f9425e1686d1244bce15
CaioCroccia <103905336+CaioCroccia@users.noreply.github.com>
2022-09-18 18:13:06 -0300

    2 files modified

--- src/app/caiosimple-interest-component/caiosimple-interest-component.component.ts
+++ src/app/caiosimple-interest-component/caiosimple-interest-component.component.ts
@@ -3,22 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-caiosimple-interest-component',
   templateUrl: './caiosimple-interest-component.component.html',
-  styleUrls: ['./caiosimple-interest-component.component.css']
+  styleUrls: ['./caiosimple-interest-component.component.css'],
 })
 export class CaiosimpleInterestComponentComponent implements OnInit {
   valor: number;
   periodo: number;
   taxa: 0.1;
+  resultado: number;
 
+  constructor() {}
 
+  ngOnInit() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  getJuros() {
+    return this.resultado = this.valor * (1 + this.taxa * this.periodo);
   }
-
-  getJuros(){
-    return this.valor * (1+ this.taxa * this.periodo);
-  }
-
 }
